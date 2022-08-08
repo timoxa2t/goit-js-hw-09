def5eff5cc69d3cc0660731ac8aaf5cf2d9863fc
timoxa2t <alex2t@ukr.net>
2022-08-08 16:33:29 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
 
+const startBtn = document.querySelector("button[data-start]")
+const stopBtn = document.querySelector("button[data-stop]")
+const body = document.querySelector("body")
+stopBtn.toggleAttribute("disabled")
+
+let timerId
+
+function switchDisabledButtons(){
+    startBtn.toggleAttribute("disabled")
+    stopBtn.toggleAttribute("disabled")
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function setBackground(){
+    body.style.background = getRandomHexColor()
+}
+
+startBtn.addEventListener("click", (event) => {
+    setBackground()
+    timerId = setInterval(setBackground, 1000)
+    switchDisabledButtons()
+})
+
+stopBtn.addEventListener("click", (event) => {
+    clearInterval(timerId)
+    timerId = null
+    switchDisabledButtons()
+})
+
+
